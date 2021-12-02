65801603d31a2764c46a1dda6721ffeccfe6a38d
Dylan <90397861+caplox@users.noreply.github.com>
2021-12-03 00:31:39 +0100

    Update main.c

--- 1_day2/main.c
+++ 1_day2/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int dayOne()
+int partOne()
 {
 	FILE * fPointer = fopen("input.txt", "r");
 	char command[50];
@@ -30,9 +30,40 @@ int dayOne()
 	}
 	return x*y;
 }
+
+int partTwo()
+{
+	FILE * fPointer = fopen("input.txt", "r");
+	char command[50];
+	int num = 0;
+	int x = 0;
+	int y = 0;
+	int aim = 0;
 	
+	for ( int i = 0; i < 1000; i++ )
+	{
+		fscanf(fPointer, "%s", &command);
+		fscanf(fPointer, "%d", &num);
+	
+		switch (strlen(command))
+		{
+				case 7:
+					x = x+num;
+					y += aim*num;
+					break;
+				case 2:
+					aim = aim-num;
+					break;
+				case 4:
+					aim = aim+num;
+					break;
+		}
+	}
+	return x*y;
+}
 
 int main()
 {
-	printf("%d", dayOne());
+	printf("%d\n", partOne());
+	printf("%d\n", partTwo());
 }
