aa532fd069f55e4580fc201f97c51bf9136a9140
Dylan <90397861+caplox@users.noreply.github.com>
2022-02-06 11:41:35 +0100

    Create main.py

---
+++ 02/main.py
@@ -0,0 +1,31 @@
+def partOne():
+    file1 = open("input.txt", "r")
+    x, y = 0, 0
+    for line in file1:
+        command = line.strip()
+        command = command.split(" ")
+        if "forward" in command:
+            x += int(command[1])
+        elif "up" in command:
+            y -= int(command[1])
+        elif "down" in command:
+            y += int(command[1])
+    return x*y
+
+def partTwo():
+    file1 = open("input.txt", "r")
+    x, y, aim = 0, 0, 0
+    for line in file1:
+        command = line.strip()
+        command = command.split(" ")
+        if "forward" in command:
+            x += int(command[1])
+            y += aim*int(command[1])
+        elif "up" in command:
+            aim -= int(command[1])
+        elif "down" in command:
+            aim += int(command[1])     
+    return x*y
+        
+print(partOne()) 
+print(partTwo())
